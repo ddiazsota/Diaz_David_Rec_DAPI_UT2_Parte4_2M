dd4049939463923711d253d1f132a05ea6006750
ddiazsota <144794611+ddiazsota@users.noreply.github.com>
2024-05-27 18:32:12 +0200

    Add files via upload

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -1,6 +1,21 @@
 import csv
 def calculate_grade(Practica01, Practica02, Practica03, Examen, 
                     Recuperacion, Actitud):
+    """ Esta función consiste en que a partir de los datos de proccess class
+    calcule la nota media de los alumnos/as y función recibirá las notas de todos
+    los apartados evaluados de cada alumno/a y devolverá unanota final y un valor Verdadero/Falso
+    en función de si ha aprobado o suspendido.
+    NotaFinal = (Practica01 + Practica02 +Practica03)/3 * 0,3 +
+    max(Examen; Recuperacion)* 0,6 + Actitud * 0,1
+    Parámetros:
+        -Fichero class.csv
+        -Seis float con las notas de cada alumno:
+         Practica01, Practica02, Practica03, Examen, Recuperacion, Actitud
+    Salida:
+        -El alumno ha superado la asignatura si/no
+    """
+
+    
     Examen2 = [Examen, Recuperacion]
     total_practicas = Practica01 + Practica02 + Practica03
     NotaFinal = total_practicas/3 * 0.3 + max(Examen2)* 0.6 + Actitud * 0.1
@@ -11,7 +26,14 @@ def calculate_grade(Practica01, Practica02, Practica03, Examen,
         return NotaFinal,False
 
 
-def gen_correo(nombre,apellido):
+def create_email(nombre,apellido):
+    """Esta función consiste en concatenar dos str y formar un correo
+    electrónico.
+    Parámetros:
+    -Dos str con el nombre y el apellido de cada alumno/a
+    Salida:
+    -Str con el email resultante
+    """
     correo=""
     correo+=nombre[0]
     Long_apellido = len(apellido)
@@ -27,6 +49,13 @@ def gen_correo(nombre,apellido):
     return correo
 
 def process_class(fichero):
+    """Esta función lee un fichero csv  con notas de los alumnos y
+    las almacena.
+        -Parámetros:
+            -Fichero = str con la ruta del fichero (.csv) a abrir
+        -Salida:
+            -No tiene
+    """
     with open(fichero, "r", newline='') as file:
         csv_clase = csv.reader(file)
         next(csv_clase)  
@@ -43,7 +72,7 @@ def process_class(fichero):
             recuperacion = float(alumno[6].strip('"').replace(',', '.'))
             actitud = float(alumno[7].strip('"').replace(',', '.'))
             
-            email = gen_correo(nombre, apellido)
+            email = create_email(nombre, apellido)
             nota_final, aprobado = calculate_grade(practica1, practica2, 
                                                    practica3, examen, recuperacion, actitud)
             
